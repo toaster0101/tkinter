f0ceb3f62d4feb9ac012fa9c82c78c6252351331
toaster0101 <henry.bray.star@gmail.com>
2026-06-19 16:56:18 +0100

    Add files via upload

--- lesson 1/what you'd expect.py
+++ lesson 1/what you'd expect.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 root=Tk()
-root.geometry("500x500")
-root.config(background="green")
-root.title("Lesson 1")
+root.geometry("150x623")
+root.config(background="purple")
+root.title("test")
 
 root.mainloop()
