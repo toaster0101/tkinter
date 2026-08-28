1c20d9de69c3783bddb72440f53530b2c9375391
toaster0101 <henry.bray.star@gmail.com>
2026-08-28 16:09:10 +0100

    Add files via upload

--- tkinter/lesson 7.5/hw.py
+++ tkinter/lesson 7.5/hw.py
@@ -3,12 +3,11 @@ from tkinter.ttk import Combobox
 import calendar
 
 root=Tk()
-root.geometry("250x300")
 root.title("months")
 
 def showcal():
-    month=str(n.get())
-    yr=yrEnt.get()
+    month=n.get()
+    yr=int(yrEnt.get())
     calcontent=calendar.month(yr,month)
     calLabel.config(text=calcontent)
     calLabel.pack()
@@ -18,14 +17,14 @@ frame1.pack()
 
 yrlbl=Label(frame1,text="Enter Year: ")
 yrlbl.grid(row=0,column=0)
-yrEnt=Entry(frame1,width=20)
+yrEnt=Entry(frame1,width=6)
 yrEnt.grid(row=0,column=1)
 
 monthLabel=Label(frame1,text="Select Month: ")
 monthLabel.grid(row=1,column=0,pady=10)
 
 n=IntVar()
-dropbox=Combobox(frame1,textvariable=n,width=15)
+dropbox=Combobox(frame1,textvariable=n,width=4)
 dropbox['values']=list(range(1,13))
 dropbox.current(0)
 dropbox.grid(row=1,column=1,pady=10)
